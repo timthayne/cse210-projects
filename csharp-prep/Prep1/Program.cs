a3f6468a69f2646f2d9a63a5441551637d293dde
Tim Thayne <timthayne@gmail.com>
2024-01-16 13:31:08 -0700

    Completed excercise 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // string concatenation
+        // Console.WriteLine("Your name is " + lastName + ", " + firstName + " " + lastName); 
+
+        // string formatting
+        // Console.WriteLine("Your name is {0}, {1} {0}", lastName, firstName);
+        
+        // string interpolation
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
